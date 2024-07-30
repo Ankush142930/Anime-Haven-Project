8da793e849cb0bfed9d7470c8d5a2c92e2675177
Ankush <ankushcuber14@gmail.com>
2024-07-31 04:01:52 +0530

    Final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Popular from './Components/Popular.jsx';
 import AnimeItem from './Components/AnimeItem.jsx';
+import HomePage from './Components/HomePage.jsx';
+import Gallery from './Components/Gallery.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Popular />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/anime/:id" element={<AnimeItem />} />
+        <Route path="/character/:id" element={<Gallery />} />
       </Routes>
     </BrowserRouter>
   );
--- src/context/global.jsx
+++ src/context/global.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useReducer, useState } from 'react';
 
 const GlobalContext = createContext();
 
@@ -14,6 +14,14 @@ const reducer = (state, action) => {
       return { ...state, loading: true };
     case 'GET_POPULAR_ANIME':
       return { ...state, popularAnime: action.payload, loading: false };
+    case 'GET_UPCOMING_ANIME':
+      return { ...state, upcomingAnime: action.payload, loading: false };
+    case 'GET_AIRING_ANIME':
+      return { ...state, airingAnime: action.payload, loading: false };
+    case 'SEARCH':
+      return { ...state, searchResult: action.payload, loading: false };
+    case 'GET_CHARACTER_PICTURES':
+      return { ...state, pictures: action.payload, loading: false };
     default:
       return state;
   }
@@ -25,6 +33,7 @@ const SEARCH = 'SEARCH';
 const GET_POPULAR_ANIME = 'GET_POPULAR_ANIME';
 const GET_UPCOMING_ANIME = 'GET_UPCOMING_ANIME';
 const GET_AIRING_ANIME = 'GET_AIRING_ANIME';
+const GET_CHARACTER_PICTURES = 'GET_CHARACTER_PICTURES';
 
 export const GlobalContextProvider = ({ children }) => {
   //Initial State of the application
@@ -39,6 +48,25 @@ export const GlobalContextProvider = ({ children }) => {
   };
 
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [search, setSearch] = useState('');
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+    if (e.target.value == '') {
+      state.isSearch = false;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (search) {
+      searchAnime(search);
+      state.isSearch = true;
+    } else {
+      state.isSearch = false;
+      alert('Please enter a valid search term');
+    }
+  };
 
   //Fetching popular anime
   const getPopularAnime = async () => {
@@ -48,13 +76,60 @@ export const GlobalContextProvider = ({ children }) => {
     dispatch({ type: GET_POPULAR_ANIME, payload: data.data });
   };
 
+  //Fetching Top-airing anime
+  const getAiringAnime = async () => {
+    dispatch({ type: LOADING });
+    const response = await fetch(`${baseURL}/top/anime?filter=airing`);
+    const data = await response.json();
+    dispatch({ type: GET_AIRING_ANIME, payload: data.data });
+  };
+
+  //Fetching upcoming anime
+  const getUpcomingAnime = async () => {
+    dispatch({ type: LOADING });
+    const response = await fetch(`${baseURL}/top/anime?filter=upcoming`);
+    const data = await response.json();
+    dispatch({ type: GET_UPCOMING_ANIME, payload: data.data });
+  };
+
+  //search an anime
+  const searchAnime = async (anime) => {
+    dispatch({ type: LOADING });
+    const response = await fetch(
+      `${baseURL}/anime?q=${anime}&order_by=popularity&sort=asc&sfw`
+    );
+    const data = await response.json();
+    console.log(data.data);
+    dispatch({ type: SEARCH, payload: data.data });
+  };
+
+  //fetching anime characters
+  const getCharacterPictures = async (id) => {
+    dispatch({ type: LOADING });
+    const response = await fetch(`${baseURL}/characters/${id}/pictures`);
+    const data = await response.json();
+    dispatch({ type: GET_CHARACTER_PICTURES, payload: data.data });
+  };
+
   //initial render
   React.useEffect(() => {
     getPopularAnime();
   }, []);
 
   return (
-    <GlobalContext.Provider value={{ ...state }}>
+    <GlobalContext.Provider
+      value={{
+        ...state,
+        handleChange,
+        handleSubmit,
+        searchAnime,
+        search,
+        getAiringAnime,
+        getUpcomingAnime,
+        getPopularAnime,
+        getCharacterPictures,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
